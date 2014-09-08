cf0485c56bef50f202c2da72edbcfa7e05c176ac
Gerhard Schöfl <gerhard.schofl@gmail.com>
2014-09-08 10:55:38 +0200

    Show mismatches to reference sequence in quality plot

--- R/alignment-quality.R
+++ R/alignment-quality.R
@@ -64,56 +64,104 @@ consensus_table <- function(bamfile, reffile) {
   res
 }
 
+
+plot_quality_line <- function(prob.tbl, match.mismatch.tbl, smooth = TRUE,
+                              annotate = TRUE, annot.size = 6, refname = "") {
+  p <- ggplot(prob.tbl, aes(x = as.numeric(Position), y = Probability.query)) +
+    geom_line(alpha = 0.75, size = 0.25, colour = "gray30") +    
+    xlab("Nucleotide position") +
+    ylab("Proportion query reads matching reference") +
+    ggtitle(bquote(atop("Alignment Quality", atop(italic(.(refname)), "")))) +
+    theme_bw()
+  if (annotate) {
+    p <- p + 
+      geom_point(data = match.mismatch.tbl, aes(x = as.numeric(Position), y = Probability.query),
+                            shape = 16, colour = "#f5410b", size = 2) +
+      geom_text(data = match.mismatch.tbl, aes(y = Probability.query - .02, label = Mismatch),
+                       size = annot.size)
+  }
+  if (smooth) {
+    p <- p + 
+      stat_smooth(method = "gam", formula = y ~ s(x))
+  }
+  p
+}
+
+
+plot_quality_box <- function(prob.tbl) {
+  p <- ggplot(prob.tbl, aes(x = 1, y = Probability.query)) + 
+    geom_boxplot() +
+    scale_color_continuous(breaks = NULL) +
+    ylab("Proportion query reads matching reference") +
+    theme(axis.title.x = element_blank()) +
+    theme_bw()
+  p
+}
+
+
+plot_depth_line <- function(prob.tbl, refname = "") {
+  p <- ggplot(prob.tbl, aes(x = as.numeric(Position), y = total_reads)) +
+    geom_line(alpha = 0.75, size = 0.25, colour = "gray30") +
+    xlab("Nucleotide position") +
+    ylab("Alignment Depth") +
+    ggtitle(bquote(atop("Alignment Depth (Number of reads/bp)", atop(italic(.(refname)), "")))) +
+    theme_bw()
+  p
+}
+
+
+plot_depth_box <- function(prob.tbl) {
+  p <- ggplot(prob.tbl, aes(x = 1, y = total_reads)) + 
+    geom_boxplot() +
+    scale_color_continuous(breaks = NULL) +
+    ylab("Alignment Depth") +
+    theme(axis.title.x = element_blank()) +
+    theme_bw()
+  p
+}
+
+
 #' Plot alignment quality.
 #' 
 #' This function graphs the probability to recover the reference sequence from
 #' an multiple alignment of nanopore reads.
 #' 
 #' @param x A \code{\link{consensus_table}}.
+#' @param annotate Annotate mismatch positions.
+#' @param annot.size If \code{annotate = TRUE}, size of the annotation text. 
 #' @param smooth Plot a smoothing function (gam: \code{y ~ s(x)}).
 #' @return Plots the probability to recover the reference sequence and summarises it in a
 #' boxplot.
 #' @export
-plot_alignment_quality <- function(x, smooth = TRUE) {
+plot_alignment_quality <- function(x, smooth = TRUE, annotate = TRUE, annot.size = 4) {
   assert_that(is(x, "data.table"), has_attr(x, "refname"))
   refname <- paste("Refname:", attr(x, "refname"))
-  prob.tbl <- x[Probability.ref == 1, list(Position, Nucleotide, Probability.query)]
-  nreads <- x[, list(Nreads = sum(Nreads)), by = "Position"]
-  prob.tbl <- prob.tbl[nreads]
-  
-  qual.line <- ggplot(prob.tbl, aes(x = as.numeric(Position), y = Probability.query)) +
-    geom_line(alpha = 0.75, size = 0.25, colour = "gray30") +
-    xlab("Nucleotide position") +
-    ylab("Fraction of queries matching reference") +
-    ggtitle(bquote(atop("Alignment Quality", atop(italic(.(refname)), "")))) +
-    theme_bw()  
-  if (smooth) {
-    qual.line <- qual.line + stat_smooth(method = "gam", formula = y ~ s(x))
-  }
-  
-  qual.box <- ggplot(prob.tbl, aes(x = 1, y = Probability.query)) + 
-    geom_boxplot() +
-    scale_color_continuous(breaks = NULL) +
-    ylab("Fraction of queries matching reference") +
-    theme(axis.title.x = element_blank()) +
-    theme_bw()
-  
-  depth.line <- ggplot(prob.tbl, aes(x = as.numeric(Position), y = Nreads)) +
-    geom_line(alpha = 0.75, size = 0.25, colour = "gray30") +
-    xlab("Nucleotide position") +
-    ylab("Alignment Depth") +
-    ggtitle(bquote(atop("Alignment Depth (Number of reads/bp)", atop(italic(.(refname)), "")))) +
-    theme_bw()
-  
-  depth.box <- ggplot(prob.tbl, aes(x = 1, y = Nreads)) + 
-    geom_boxplot() +
-    scale_color_continuous(breaks = NULL) +
-    ylab("Alignment Depth") +
-    theme(axis.title.x = element_blank()) +
-    theme_bw()  
-  
+  ## extract the proportion of query reads matching the reference sequence for
+  ## each base position.
+  prob.tbl <- x[Probability.ref == 1L, list(Position, Nucleotide, Probability.query)]
+  ## aggregate the total number of reads for each positions.
+  total_reads <- x[, list(total_reads = sum(Nreads)), by = "Position"]
+  prob.tbl <- prob.tbl[total_reads]
+  ## A Match-Mismatch table comparing the "majority rule" consensus sequence
+  ## to the reference
+  mismatch.tbl <- x[, .SD[which.max(Probability.query)], by = "Position"][Probability.ref == 0]
+  setkey(mismatch.tbl, "Position")
+  match.mismatch.tbl <- 
+    x[Probability.ref == 1][mismatch.tbl][, list(
+      Position,
+      Probability.ref = Probability.query,
+      Probability.query = Probability.query.1,
+      Mismatch = paste(Nucleotide, Nucleotide.1, sep = "/")
+    )]
+  ## Plots
+  pdl <- plot_depth_line(prob.tbl, refname)
+  pdb <- plot_depth_box(prob.tbl)
+  pql <- plot_quality_line(prob.tbl, match.mismatch.tbl, smooth,
+                           annotate, annot.size, refname)
+  pqb <- plot_quality_box(prob.tbl)
+  ## Multiplot
   lay <- matrix(c(1,1,1,2,3,3,3,4), nrow = 2, byrow = TRUE)
-  multiplot(depth.line, depth.box, qual.line, qual.box, layout = lay)
+  multiplot(pdl, pdb, pql, pqb, layout = lay)
   
-  invisible(NULL)
+  invisible(list(pql = pql, pdl = pdl, match.mismatch.tbl = match.mismatch.tbl))
 }
